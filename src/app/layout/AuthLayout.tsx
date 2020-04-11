a21427ecf2fb6322e72dc4761dab32e415c280bf
embarq <embarq.dev@gmail.com>
2020-04-11 19:34:17 +0300

    feat(ui): impl auth section and login page ui

--- src/app/layout/AuthLayout.tsx
+++ src/app/layout/AuthLayout.tsx
@@ -2,20 +2,57 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect,
+  useRouteMatch
 } from "react-router-dom";
 
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+import RegisterSuccessPage from '../pages/RegisterSuccessPage';
 import './AuthLayout.css';
 
-export type AuthLayoutProps = unknown;
+interface AuthLayoutProps {
+  children: React.ReactNode
+}
 
-export function AuthLayout(props: AuthLayoutProps) {
+function AuthLayout(props: AuthLayoutProps) {
   return (
-    <div>
-      <pre>AuthLayout</pre>
-      
+    <div className="auth-main-wrapper">
+      <div className="container">
+        <div className="auth-content">
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default AuthLayout;
+export type AuthLayoutContainerProps = unknown;
+
+function AuthLayoutContainer(props: AuthLayoutContainerProps) {
+  const { path } = useRouteMatch();
+
+  return (
+    <Router>
+      <AuthLayout>
+        <Switch>
+          <Route exact path={`${ path }/login`}>
+            <LoginPage />
+          </Route>
+          <Route exact path={`${ path }/register`}>
+            <RegisterPage />
+          </Route>
+          <Route exact path={`${ path }/success`}>
+            <RegisterSuccessPage />
+          </Route>
+          <Route exact path={path}>
+            <Redirect to={`${ path }/login`}></Redirect>
+          </Route>
+        </Switch>
+      </AuthLayout>
+    </Router>
+  );
+}
+
+export default AuthLayoutContainer;
--- src/app/pages/LoginPage.tsx
+++ src/app/pages/LoginPage.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LoginPage.css';
 
-function LoginPage(props: React.PropsWithChildren<{}>) {
+function LoginPage() {
   return (
-    <div>
-      <pre>LoginPage</pre>
-      {props.children}
+    <div className="card border-0 login-content-card">
+      <div className="card-header bg-white border-0">
+        <h1 className="mt-3">Login</h1>
+      </div>
+      <div className="card-body flex-1">
+        <form action="">
+          <fieldset className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" autoComplete="off" placeholder="example@org.com" className="form-control"/>
+          </fieldset>
+          <fieldset className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="text" id="password" autoComplete="off" placeholder="••••••••" className="form-control"/>
+          </fieldset>
+        </form>
+      </div>
+      <div className="card-footer bg-white border-0">
+        <div className="d-flex justify-content-between">
+          <Link to="/auth/register" className="btn btn-info">Register</Link>
+          <button className="btn btn-primary">Login</button>
+        </div>
+        <div className="text-center py-3">
+          <button className="btn btn-link">Forgot password? Click here to get help</button>
+        </div>
+      </div>
     </div>
   );
 }
