f09367dc98e909a12255bd5907ec2740c388c85c
embarq <embarq.dev@gmail.com>
2020-04-11 21:26:08 +0300

    feat(chore): impl FirebaseService

---
+++ src/app/lib/firebase-service.ts
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+import * as CONFIG from '../../.runtimeconfig.json';
+
+type FirebaseApp = firebase.app.App;
+
+export class FirebaseService {
+  private app: FirebaseApp;
+
+  constructor(appConfig: any) {
+    this.app = firebase.initializeApp(appConfig);
+  }
+
+  public getAuth() {
+    return this.app.auth();
+  }
+
+  static Instance = new FirebaseService(CONFIG['webapp_firebase_config']);
+}
